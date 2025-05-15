bddf1177a5ed4d427d878cb09b21339742b97588
小九 <96114146+yixiaoleng@users.noreply.github.com>
2025-05-15 18:13:54 +0800

    Add files via upload

---
+++ Kernel.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console;
+
+use App\Models\Order;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        $schedule->call(function () {
+            $timestamp = now()->timestamp;
+            Order::query()->whereNotNull('freeze_expired_at')->where('freeze_expired_at', '<=', $timestamp)
+                ->chunk(100, function ($orders) {
+                    foreach ($orders as $order) {
+                        \product_storehouse_order_free_up($order->id);
+                    }
+                });
+        })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
